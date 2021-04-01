a4e973fda33eec77206f425ccb35548cb5461cfd
sagostinelli <batonenbois@gmail.com>
2021-04-01 12:07:27 -0400

    test

--- src/internet_computer_tutorial_assets/public/index.js
+++ src/internet_computer_tutorial_assets/public/index.js
@@ -1,5 +1,5 @@
 import internet_computer_tutorial from 'ic:canisters/internet_computer_tutorial';
 
-internet_computer_tutorial.greet(window.prompt("Enter your AWESOME name:")).then(greeting => {
+internet_computer_tutorial.greet(window.prompt("Enter your AWESOME name :P :")).then(greeting => {
   window.alert(greeting);
 });
